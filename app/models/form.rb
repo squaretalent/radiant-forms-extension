185ab9a068c40f26d422d5475d78dd6539266dec
Dirk Kelly <dk@dirkkelly.com>
2010-09-30 22:33:29 +0800

    created_by and updated_by on forms

--- app/models/form.rb
+++ app/models/form.rb
@@ -3,4 +3,7 @@ class Form < ActiveRecord::Base
   validates_presence_of :title
   validates_uniqueness_of :title
   
+  belongs_to :created_by, :class_name => 'User'
+  belongs_to :updated_by, :class_name => 'User'
+  
 end
