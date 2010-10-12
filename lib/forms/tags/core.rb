f262bab6291266855d5be0d71ed3b27f5922f8e7
nu-ex <nx@nu-ex.com>
2010-10-12 11:15:35 -0400

    added textarea tag

--- lib/forms/tags/core.rb
+++ lib/forms/tags/core.rb
@@ -4,11 +4,11 @@ module Forms
   module Tags
     module Core
       include Radiant::Taggable
-      
+
       desc %{
         Render a form and its contents
         <pre><code><r:form name="object[key]" [method, url] /></code></pre>
-        
+
         * *name* the name of the form
         * *method* url submission method [ post, get, delete, put ]
         * *url* address to submit the form to
@@ -19,7 +19,7 @@ module Forms
             tag.attr['id'] ||= 'form_' + tag.locals.form.title
             tag.attr['method'] ||= 'put'
             tag.attr['action'] ||= tag.locals.form.action.blank? ? "/forms/#{tag.locals.form.id}" : tag.locals.form.action
-            
+
             parse(Forms::Tags::Helpers.render(tag))
           else
             raise TagError, "Could not find '#{tag.attr['name']}' form"
@@ -28,32 +28,32 @@ module Forms
           tag.expand
         end
       end
-      
-      desc %{ 
+
+      desc %{
         Render a @<label />@ tag to be used inside a form
         <pre><code><r:form:label for="object[key]" /></code></pre>
-        
+
         **Required**
         * *for* the name of the field you are relating the tag to
-        
+
         _A label is a piece of text which activates its related input when clicked_
       }
       tag 'form:label' do |tag|
         Forms::Tags::Helpers.require!(tag,'form:label','for')
-        
+
         tag.attr['for'] = tag.attr['for'].gsub('[','_').gsub(']','') unless tag.attr['for'].nil?
-        
+
         %(<label #{Forms::Tags::Helpers.attributes(tag)}>#{tag.expand}</label>)
       end
-      
+
       %w(text password reset checkbox radio hidden file button).each do |type|
         desc %{
           Render a @<#{type}>...</#{type}>@ tag to be used in a form
           <pre><code><r:form:#{type} name="object[key]" /></code></pre>
-          
+
           **Required**
           * *name* the name of the data to be sent
-          
+
           **Optional**
           * *class* css class names
           * *placeholder* default text, which is cleared when clicked
@@ -61,84 +61,100 @@ module Forms
         }
         tag "form:#{type}" do |tag|
           Forms::Tags::Helpers.require!(tag,"form:#{type}",'name')
-          
+
           value = tag.attr['value']
           tag.attr['type'] = type
-          
+
           %(<input type="#{type}" value="#{value}" #{Forms::Tags::Helpers.attributes(tag)} />)
         end
       end
-      
+
       desc %{
-        Renders a @<select>...</select>@ tag to be used in a form
-        
-        <pre><code><r:form:select name="object[key]"><r:option value="text" /></r:form:select></code></pre>
-        
+        Render a @<textarea>..</textarea>@ tag to be used in a form
+        <pre><code<r:form:textarea name="object[key]">...</r:form:textarea></code></pre>
+
         **Required**
         * *name* the name of the data to be sent
-        
+
         **Optional**
         * *class* css class names
-        
+      }
+      tag 'form:textarea' do |tag|
+        Forms::Tags::Helpers.require!(tag,"form:textarea",'name')
+
+        %{<textarea #{Forms::Tags::Helpers.attributes(tag)}>#{tag.expand}</textarea>}
+      end
+
+      desc %{
+        Renders a @<select>...</select>@ tag to be used in a form
+
+        <pre><code><r:form:select name="object[key]"><r:option value="text" /></r:form:select></code></pre>
+
+        **Required**
+        * *name* the name of the data to be sent
+
+        **Optional**
+        * *class* css class names
+
         _@<r:option />@ tags may be nested inside the tag to automatically generate options_
       }
       tag 'form:select' do |tag|
         Forms::Tags::Helpers.require!(tag,'form:select','name')
-        
+
         tag.locals.parent_tag_name = tag.attr['name']
         tag.locals.parent_tag_type = 'select'
-        
+
         %(<select #{Forms::Tags::Helpers.attributes(tag)}>#{tag.expand}</select>)
       end
-      
+
       desc %{
         Renders a series of @<input type="radio"/>@ tags to be used in a form
-        
+
         <pre><code><r:form:radios name="object[value]" value="text"><r:option value="text" /></r:form:radios></code></pre>
-        
+
         **Required**
         * *name* the name of the data to be sent
-        
+
         **Optional**
         * *class* css class names
-        
+
         _@<r:option />@ tags may be nested inside the tag to automatically generate options_
       }
       tag 'form:radios' do |tag|
         Forms::Tags::Helpers.require!(tag,'form:radios','name')
-        
+
         tag.locals.parent_tag_name = tag.attr['name']
         tag.locals.parent_tag_type = 'radios'
         tag.expand
       end
-      
+
       desc %{
         Renders an @<option/>@ tag if the parent is a
         @<r:form:select>...</r:form:select>@ or @<r:form:radios>...</r:form:radios>@
-        
+
         **Required**
         * *value* the value you want to be sent if selected
       }
       tag 'form:option' do |tag|
         value = tag.attr['value'] || tag.expand
-        
+
         if tag.locals.parent_tag_type == 'select'
           text = tag.expand.length == 0 ? value : tag.expand
-          
+
           extra = { 'value' => value}
           %(<option #{Forms::Tags::Helpers.attributes(tag, extra)}>#{text}</option>)
         elsif tag.locals.parent_tag_type == 'radios'
           name= tag.locals.parent_tag_type == 'radios' ? tag.locals.parent_tag_name : tag.attr['name']
           id  = name.gsub('[','_').gsub(']','') + '_' + value
-          
+
           extra = { 'id' => id, 'value' => value, 'name' => name, 'type' => 'radio' }
           %(<input #{Forms::Tags::Helpers.attributes(tag, extra)} />)
         end
       end
-      
+
       desc %{
         Renders an @<input type="submit" />@ tag for a form.
-        
+
         **Optional**
         * *value* the text on the submit
       }
@@ -147,53 +163,53 @@ module Forms
         extra = { 'value' => value, 'type' => 'submit', 'class' => 'submit', 'name' => 'submit' }
         %(<input #{Forms::Tags::Helpers.attributes(tag, extra)} />)
       end
-      
+
       desc %{
         Query the data from a submitted form
-        
+
         @<r:form:read name="object[value]" />@ access the object which was submitted
-        
+
         _This can only be used in the content section of a form, use @<r:form:get />@ in pages_
       }
       tag 'form:read' do |tag|
         Forms::Tags::Helpers.require!(tag,'form:read','name')
         data = Forms::Tags::Responses.retrieve(tag.locals.page.data, tag.attr['name'])
       end
-      
+
       desc %{
         Clears the response object
-        
+
         @<r:response:clear />@
       }
       tag 'response:clear' do |tag|
         Forms::Tags::Responses.clear(tag)
-        
+
         nil
       end
-      
-      desc %{ 
+
+      desc %{
         Allows access to the response of a submitted form
         @<r:response>...<r:get /><r:clear />...</r:response>@
-        
+
         _Will not expand if a form has not been submitted or has been cleared_
       }
       tag 'response' do |tag|
         tag.locals.response = Forms::Tags::Responses.current(tag,request)
-        
+
         tag.expand if tag.locals.response.present? and tag.locals.response.result.present?
       end
-      
-      desc %{ 
+
+      desc %{
         Access the attributes of a submitted form.
-        
+
         @<r:response:get name="object[value]" />@ an object which was submitted with the form
         @<r:response:get name="result[mail][value]" />@ a response to the mail extension hooking the form
-        
+
         _Refer to the readme on extensions to find out what they return_
       }
       tag 'response:get' do |tag|
         Forms::Tags::Helpers.require!(tag,'response:get','name')
-        
+
         result = Forms::Tags::Responses.retrieve(tag.locals.response.result, tag.attr['name']).to_s
       end
     end
--- spec/lib/forms/tags_spec.rb
+++ spec/lib/forms/tags_spec.rb
@@ -2,14 +2,14 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe Forms::Tags do
   dataset :pages, :forms
-  
+
   context 'form output' do
-    
+
     before :all do
       @form = forms(:test)
       @page = pages(:home)
     end
-    
+
     describe '<r:form>' do
       it 'should render the correct HTML' do
         tag = %{<r:form name="#{@form.title}" />}
@@ -21,91 +21,106 @@ describe Forms::Tags do
        @page.should render(tag).as(expected)
       end
     end
-    
+
     describe '<r:form:label>' do
-      
+
       it 'should render the correct HTML' do
         tag = %{<r:form:label for="test[field]">text</r:form:label>}
         expected = %{<label id="test_field_label" for="test_field">text</label>}
         @page.should render(tag).as(expected)
       end
-      
+
       it 'should require the name attribute' do
         tag = %{<r:form:label />}
         @page.should_not render(tag)
       end
-      
+
     end
 
     %w(text password reset checkbox radio hidden file button).each do |type|
 
-      describe "<r:form:#{type}>" do      
-        
+      describe "<r:form:#{type}>" do
+
         it 'should render the correct HTML' do
           tag = %{<r:form:#{type} name="test[field]">text</r:form:#{type}>}
           expected = %{<input type="#{type}" value="" name="test[field]" class="#{type}" id="test_field" />}
           @page.should render(tag).as(expected)
         end
-        
+
         it 'should require the name attribute' do
           tag = %{<r:form:#{type} />}
           @page.should_not render(tag)
         end
-        
+
       end
-      
+
     end
-    
+
+    describe '<r:form:textarea>' do
+
+      it 'should render the correct HTML' do
+        tag = %{<r:form:textarea name="test[field]">test</r:form:textarea>}
+        expected = %{<textarea name="test[field]" id="test_field">test</textarea>}
+        @page.should render(tag).as(expected)
+      end
+
+      it 'should require the name attribute' do
+        tag = %{<r:form:textarea></r:form:textarea>}
+        @page.should_not render(tag)
+      end
+
+    end
+
     describe '<r:form:select>' do
-      
+
       it 'should render the correct HTML' do
         tag = %{<r:form:select name="test[field]" />}
         expected = %{<select name="test[field]" id="test_field"></select>}
         @page.should render(tag).as(expected)
       end
-      
+
       it 'should render correct options HTML' do
         tag = %{<r:form:select name="test[field]"><r:option value="one" /><r:option value="two">three</r:option></r:form:select>}
         expected = %{<select name="test[field]" id="test_field"><option value="one">one</option><option value="two">three</option></select>}
         @page.should render(tag).as(expected)
       end
-      
+
       it 'should require the name attribute' do
         tag = %{<r:form:select />}
         @page.should_not render(tag)
       end
-      
+
     end
-    
+
     describe '<r:form:radios>' do
-      
+
       it 'should render the correct HTML' do
         tag = %{<r:form:radios name="test[field]" />}
         expected = %{}
         @page.should render(tag).as(expected)
       end
-      
+
       it 'should render correct options HTML' do
         tag = %{<r:form:radios name="test[field]"><r:option value="one" /><r:option value="two" /></r:form:radios>}
         expected = %{<input name="test[field]" id="test_field_one" type="radio" value="one" /><input name="test[field]" id="test_field_two" type="radio" value="two" />}
         @page.should render(tag).as(expected)
       end
-      
+
       it 'should require the name attribute' do
         tag = %{<r:form:radios />}
         @page.should_not render(tag)
       end
-      
+
     end
-    
+
     describe '<r:form:submit>' do
-      
+
       it 'should render the correct HTML' do
-        
+
       end
-      
+
     end
-    
+
   end
-  
+
 end
