4fe42a4289615778d4c5d2f2a5d594154b0f50b9
Dirk Kelly <dk@dirkkelly.com>
2010-10-23 15:19:20 +0800

    avoiding exceptions in the case where a response object is deleted and a session is still using that response id

--- lib/forms/controllers/application_controller.rb
+++ lib/forms/controllers/application_controller.rb
@@ -6,14 +6,16 @@ module Forms
         base.class_eval do
           def current_response
             return @current_response if defined?(@current_response)
-            @current_response = find_response
+            @current_response = find_or_create_response if request.session[:form_response]
           end
           
           def find_response
             response = nil
             
-            if request.session[:form_response]
+            begin
               response = Response.find(request.session[:form_response])
+            rescue
+              response = nil
             end
                         
             response
