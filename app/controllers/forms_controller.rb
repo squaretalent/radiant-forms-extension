58fa9ec83f473ead65b8f372ff1eb78993879ff6
Dirk Kelly <dk@dirkkelly.com>
2010-11-22 18:02:05 +0800

    Attempting to tidy up the forms controller. It's still horrible and in serious need of a programmer's touch.

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -12,28 +12,52 @@ class FormsController < ApplicationController
   end
 
   def update
+    set_page
+    set_form
+    set_response
+    set_configuration
+    
+    execute_extensions
+    
+    begin
+      @response.save!
+      redirect_to @form.redirect_to.present? ? @form.redirect_to : @page.url
+    rescue
+      "Form '#{@form.title}' could not be submitted."
+    end
+  end
+  
+protected
+
+  def set_page
     @page = Page.find(params[:page_id]) rescue Page.first
     @page.data    = params 
     @page.request = OpenStruct.new({
       :referrer => request.referer,
       :session => session # Creating a pretend response object
     })
-    
+  end
+  
+  def set_form
     @form = Form.find(params[:id])
     @form.page = @page
-    
-    # We need a response object
+  end
+  
+  def set_response
     @response = find_or_create_response
-    # Put the submitted data into the response object
     @response.result = Forms::Config.deep_symbolize_keys(params)
-    
+  end
+  
+  def set_configuration
     begin
       # Grab the form configuration data
       @form[:extensions] = Forms::Config.convert(@form.config)
     rescue
       raise "Form '#{@form.title}' has not been configured"
     end
-    
+  end
+  
+  def execute_extensions
     @results =  {}
     # Iterate through each configured extension
     @form[:extensions].each do |name, config|
@@ -44,13 +68,6 @@ class FormsController < ApplicationController
     end
     # Those results are merged into the response object
     @response.result = @response.result.merge!({ :results => @results})
-    
-    begin
-      @response.save!
-      redirect_to @form.redirect_to.present? ? @form.redirect_to : @page.url
-    rescue
-      "Form '#{@form.title}' could not be submitted."
-    end
   end
   
 end
